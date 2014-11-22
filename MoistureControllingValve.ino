e08fd5fbf9dcaa0c29b501205c63dd179eebb27b
Gnanasenthil Ganesh <gnanasenthil654321@gmail.com>
2014-11-22 15:09:50 +0530

    Update MoistureControllingValve.ino

--- MoistureControllingValve.ino
+++ MoistureControllingValve.ino
@@ -1,3 +1,9 @@
+// This sketch is used for automatic garden watering project.
+// The arduino pin number 13 is used to send the digital output to the power transistor that switches the solenoid valve on and off
+// The Soil Moisture data is fed into the arduino via the analogue pin A0
+// The Soil Moisture data is fed only once in few hours and if the soil is found to be dry the solenoid is switched on for a few seconds (This depends
+// on the diameter of the pipe and pressure head of the water supply)
+
 int solenoid = 13;
 // assuming the power transitor connected to the solenoid is controlled through digital pin 13
 
